9cc59daa0c5d0bb4f661435a47ad5fe157c7730d
Elizabeth <elizabeth.olamisan@outlook.com>
2017-11-02 03:16:25 +0300

    Implemented UnitOfWork class

--- MultipleShops.Domain/Concrete/UnifOfWork.cs
+++ MultipleShops.Domain/Concrete/UnifOfWork.cs
@@ -3,12 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MultipleShops.Domain.Entities;
+using MultipleShops.Domain.Abstract;
 
 namespace MultipleShops.Domain.Concrete
 {
     public class UnifOfWork
     {
-        private 
+        private EFDbContext dbContext = new EFDbContext();
+
+        private Repository<Shop> shopRepository;
+        private Repository<Product> productRepository;
+
+        public IRepository<Shop> ShopRepository
+        {
+            get
+            {
+                if (shopRepository == null)
+                {
+                    shopRepository = new Repository<Shop>(dbContext);
+                }
+                return shopRepository;
+            }
+        }
+
+        public IRepository<Product> ProductRepository
+        {
+            get
+            {
+                if(productRepository == null)
+                {
+                    productRepository = new Repository<Product>(dbContext);
+                }
+                return productRepository;
+            }
+        }
+
+        public void Commit()
+        {
+            dbContext.SaveChanges();
+        }
 
     }
 }
